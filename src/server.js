5e8fd056460e5c31bd6b6681506f5c5da2ac281b
shurik409 <shurik409@gmail.com>
2019-01-27 16:46:22 +0300

    severjs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,20 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
- 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port );
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + '/dist/'));
+
+app.get(/.*/, function(req, res){
+    res.sendfile(__dirname+'/dist/index.html');
 });
+
+app.listen(port);
+
+console.log('Server sarted');
+
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+// server.listen(server_port, server_ip_address, function () {
+//   console.log( "Listening on " + server_ip_address + ", port " + server_port );
+// });
